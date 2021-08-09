e9897092c6ad03dd0e1c62b9eef1a48c9c2af3de
Thiago Brito <britotr@gmail.com>
2021-08-09 00:22:14 -0300

    Setting up tests with Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+  return (
+    <main>
+      <h1>Gatsby do Zero ao Avançado</h1>
+    </main>
+  )
+}
