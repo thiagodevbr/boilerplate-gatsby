5c72e736311bc55be9be154c920d15634fa6da0b
Thiago Brito <britotr@gmail.com>
2021-08-10 01:41:00 -0300

    Adjust in withGlobalStyles file

--- .storybook/withGlobalStyles.tsx
+++ .storybook/withGlobalStyles.tsx
@@ -1,9 +1,11 @@
 import { StoryFn } from '@storybook/addons'
 import { GlobalStyles } from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../src/styles/theme'
 
 export const withGlobalStyles = (storyFn: StoryFn) => (
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     {storyFn()}
-  </>
+  </ThemeProvider>
 )
